8581e3160ae60d10cd44f711542a7d84396d1b21
ejazsidhu <ejazsidhu@gmail.com>
2019-03-05 12:48:30 +0500

    togge iocn set and side nav title changed

--- src/app/layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/components/sidebar/sidebar.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
     public showMenu: string;
     constructor() {}
+    toggleValue = true;
 
     ngOnInit() {
         this.showMenu = '';
