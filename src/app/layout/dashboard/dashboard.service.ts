14e6905931a8fe212478a55cd04268bd6728a73d
ejazsidhu <ejazsidhu@gmail.com>
2019-03-06 11:51:56 +0500

    envoirment is added

--- src/app/layout/dashboard/dashboard.service.ts
+++ src/app/layout/dashboard/dashboard.service.ts
@@ -5,6 +5,23 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class DashboardService {
+  ip: any = '';
+  user = 0;
 
   constructor(private http: HttpClient) { }
+
+  login(cradentials: any) {
+
+    const url = this.ip + 'pictureLogin';
+    return this.http.post(url, cradentials);
+
+  }
+
+  getZone() {
+    const filter = JSON.stringify({ act: 0, userId: this.user });
+    const url = this.ip + 'loadFilters';
+    return this.http.post(url, filter);
+
+  }
 }
+
