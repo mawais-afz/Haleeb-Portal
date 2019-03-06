b3d0b00697c0fa2c77900bf07f8bbe50e4687f41
ejazsidhu <ejazsidhu@gmail.com>
2019-03-06 11:21:58 +0500

    mat selec option added into dashboard

--- src/app/layout/dashboard/dashboard.module.ts
+++ src/app/layout/dashboard/dashboard.module.ts
@@ -1,18 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule, MatCardModule, MatIconModule, MatTableModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatIconModule, MatTableModule,
+    MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
 import { MatGridListModule } from '@angular/material/grid-list';
 
 import { StatModule } from '../../shared/modules/stat/stat.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     imports: [
+        HttpClientModule,
         CommonModule,
         DashboardRoutingModule,
         MatGridListModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
         StatModule,
         MatCardModule,
         MatCardModule,
