4dc244ebccb94f2092f927916f9756294c109ef1
Muhammad Awais <mawais.afz@outloo.com>
2019-05-22 16:03:09 +0500

    product list column order change + yes no list changed for must_have

--- src/app/layout/dashboard/dashboard-data.service.ts
+++ src/app/layout/dashboard/dashboard-data.service.ts
@@ -8,7 +8,7 @@ export class DashboardDataService {
 
   lastVisit = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   YesNo = [{ key: 'n', value: 'No' }, { key: 'y', value: 'YES' }];
-  YesNoAll = [{ key: '', value: 'All' }, { key: 'Y', value: 'YES' }, { key: 'N', value: 'No' } ];
+  YesNoAll = [{ key: '', value: 'ALL SKU' }, { key: 'Y', value: 'MSL' } ];
   impactType = [{ key: '1', value: 'Manual Impact' }, { key: '2', value: 'Absentee Impact' }];
 
   getLastVisit() {
