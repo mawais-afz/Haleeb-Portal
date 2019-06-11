0a79f210d36482c3d617d5631af4f35ae9863ef6
Muhammad Awais <mawais.afz@outloo.com>
2019-06-11 12:15:29 +0500

    hash url add to shop detail (card view) page

--- src/app/layout/dashboard/innerComponents/shop-detail/shop-detail.component.ts
+++ src/app/layout/dashboard/innerComponents/shop-detail/shop-detail.component.ts
@@ -30,7 +30,7 @@ export class ShopDetailComponent implements OnInit {
     this.childModal.show();
   }
   goToEvaluation(id) {
-  window.open(`/#/dashboard/evaluation/list/details/${id}?location=shop`,'_blank')
+  window.open(`${environment.hash}dashboard/evaluation/list/details/${id}?location=shop`,'_blank')
   }
   hideChildModal(): void {
     this.childModal.hide();
