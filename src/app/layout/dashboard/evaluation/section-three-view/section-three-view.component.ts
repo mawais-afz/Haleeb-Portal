0ed1b628ef93151a5ec365ec9f0a6dae57258fcd
ejazsidhu <ejazsidhu@gmail.com>
2019-06-25 11:53:31 +0500

    toggle mslt is fixed

--- src/app/layout/dashboard/evaluation/section-three-view/section-three-view.component.ts
+++ src/app/layout/dashboard/evaluation/section-three-view/section-three-view.component.ts
@@ -103,7 +103,7 @@ export class SectionThreeViewComponent implements OnInit {
           var i=this.products.findIndex(p=>p.id==key);
           let obj={
             id:e.id,
-            available_sku:(e.available_sku =='Yes')?e.available_sku ='No':e.available_sku ='Yes',
+            available_sku:(e.available_sku==0)?e.available_sku= 1:e.available_sku =0,
             MSL:e.MSL,
             product_title:e.product_title,
             category_title:e.category_title,
