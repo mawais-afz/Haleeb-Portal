e6a0faf8eba468c52bd215ab244120d5f6e53fbf
Sikandar Niaz <sikandar.niaz@marksman.com.pk>
2020-04-02 11:53:20 +0500

    haleeb changes

--- src/app/layout/dashboard/innerComponents/home/home.component.ts
+++ src/app/layout/dashboard/innerComponents/home/home.component.ts
@@ -5,7 +5,7 @@ import { ChartType, ChartOptions } from 'chart.js';
 import * as moment from 'moment';
 declare const google: any;
 // tslint:disable-next-line: import-blacklist
-import { Observable,interval } from 'rxjs';
+import { Observable, interval } from 'rxjs';
 
 import { Router } from '@angular/router';
 import { checkAndUpdateTextDynamic } from '@angular/core/src/view/text';
@@ -22,36 +22,24 @@ import { positionElements } from 'ngx-bootstrap';
 
 export class HomeComponent implements OnInit {
 
+
+  constructor(private httpService: DashboardService, private router: Router) { }
+
   tabsData: any = [];
   loading = true;
-  date=Date();
+  date = Date();
 
 
-  constructor(private httpService: DashboardService,private router:Router) { }
 
-  ngOnInit() {
-    this.getData();
-    interval(300000).subscribe(i=>{this.getData()})
-    this.httpService.checkDate();
-    // this.initMap()
-    let userType=JSON.parse(localStorage.getItem("user_type"))
 
-    if(userType==16){
-      this.router.navigate(['/dashboard/merchandiser_List'])
-    }
-  }
-
-  
-
-  
 // doughnut chart
-public doughnutChartLabels: any[] = ['CBL', 'Competition',];
+public doughnutChartLabels: any[] = ['Haleeb', 'Competition', ];
   public doughnutChartData: any = [
     [350, 450]
   ];
   public doughnutChartType: ChartType = 'doughnut';
 
-  public doughnutChartOptions:any={
+  public doughnutChartOptions: any = {
     type: 'doughnut',
     legend: {
       position: 'right',
@@ -59,13 +47,13 @@ public doughnutChartLabels: any[] = ['CBL', 'Competition',];
     tooltips: {
       callbacks: {
         afterLabel: function(tooltipItem, data) {
-          var dataset = data['datasets'][0];
-          var percent = Math.round((dataset['data'][tooltipItem['index']]))
+          const dataset = data['datasets'][0];
+          const percent = Math.round((dataset['data'][tooltipItem['index']]));
           return '(' + percent + '%)';
         }
       }
     }
-  }
+  };
 // doughnut chart end
 // pi cahrt
 public pieChartOptions: ChartOptions = {
@@ -83,13 +71,13 @@ public pieChartOptions: ChartOptions = {
       //   return data['datasets'][0]['data'][tooltipItem['index']];
       // },
       afterLabel: function(tooltipItem, data) {
-        var dataset = data['datasets'][0];
-        var percent = Math.round((dataset['data'][tooltipItem['index']]))
+        const dataset = data['datasets'][0];
+        const percent = Math.round((dataset['data'][tooltipItem['index']]));
         return '(' + percent + '%)';
       }
     }
   },
-  // plugins: {   
+  // plugins: {
   //   datalabels: {
   //     formatter: (value, ctx) => {
   //       const label = ctx.chart.data.labels[ctx.dataIndex];
@@ -99,7 +87,7 @@ public pieChartOptions: ChartOptions = {
   // }
 };
 public pieChartLabels: any[] = [['MSL'], ['OOS']];
-public pieChartData: number[] = [67,100];
+public pieChartData: number[] = [67, 100];
 // public pieChartLabels2: any[] = [['CBL'], ['Competition']];
 // public pieChartData2: number[] = [45,100];
 public pieChartType: ChartType = 'pie';
@@ -107,15 +95,27 @@ public pieChartLegend = true;
 // public pieChartPlugins = [pluginDataLabels];
 public pieChartColors = [
   {
-    // 
-    backgroundColor: ['#AFFCAF','#FCAFAF'],
+    //
+    backgroundColor: ['#AFFCAF', '#FCAFAF'],
   },
 ];
 public pieChartColors2 = [
   {
-    backgroundColor: [ '#FFA726','#00B8F0'],
+    backgroundColor: [ '#FFA726', '#00B8F0'],
   },
 ];
+
+  ngOnInit() {
+    this.getData();
+    interval(300000).subscribe(i => {this.getData(); });
+    this.httpService.checkDate();
+    // this.initMap()
+    const userType = JSON.parse(localStorage.getItem('user_type'));
+
+    if (userType === 16) {
+      this.router.navigate(['/dashboard/merchandiser_List']);
+    }
+  }
 public chartClicked( e: any ): void {
   if (e.active.length > 0) {
     const chart = e.active[0]._chart;
@@ -134,30 +134,30 @@ public chartClicked( e: any ): void {
 // pie chart end
 
 
- 
+
 
   getData() {
-    this.tabsData=[];
-    this.loading=true;
-    let d=Date();
-    let obj: any = {
-      typeId:1,
+    this.tabsData = [];
+    this.loading = true;
+    const d = Date();
+    const obj: any = {
+      typeId: 1,
       startDate: moment(d).format('YYYY-MM-DD'),
       endDate: moment(d).format('YYYY-MM-DD'),
-    userId:localStorage.getItem('user_id'),
-    }
+    userId: localStorage.getItem('user_id'),
+    };
     this.httpService.getDashboardData(obj).subscribe(data => {
       console.log(data, 'home data');
       this.tabsData = data;
       this.loading = false;
 
-      this.pieChartData=[this.tabsData.msl,100-this.tabsData.msl];
-      this.doughnutChartData=[this.tabsData.sos,100-this.tabsData.sos]
+      this.pieChartData = [this.tabsData.msl, 100 - this.tabsData.msl];
+      this.doughnutChartData = [this.tabsData.sos, 100 - this.tabsData.sos];
 
     }, error => {
-      console.log(error, 'home error')
+      console.log(error, 'home error');
 
-    })
+    });
 
   }
 
@@ -175,7 +175,7 @@ public chartClicked( e: any ): void {
   //   // var marker = new google.maps.Marker({position: marksman, map: map});
   //   var infowindow = new google.maps.InfoWindow();
   //   var marker, i ,markserList;
-  //   for (i = 0; i < locations.length; i++) {  
+  //   for (i = 0; i < locations.length; i++) {
   //     var url="http://maps.google.com/mapfiles/ms/icons/red-dot.png"
   //     if(i==2){
   //       url="http://maps.google.com/mapfiles/ms/icons/green-dot.png";
@@ -204,7 +204,7 @@ public chartClicked( e: any ): void {
   //       });
   //     }
 
-      
+
   //     google.maps.event.addListener(marker, 'click', (function(marker, i) {
   //       return function() {
   //         infowindow.setContent(locations[i][0]);
@@ -212,7 +212,7 @@ public chartClicked( e: any ): void {
   //       }
   //     })(marker, i));
 
-     
+
 
   //     // marker.Circle.bindTo(new google.maps.LatLng(locations[i][1], locations[i][2]), marker, new google.maps.LatLng(locations[i][1], locations[i][2]));
   //   }
@@ -221,8 +221,8 @@ public chartClicked( e: any ): void {
   //   // for (var j = 0; j < markserList.length; j++) {
   //   //  bounds.extend(markserList[j].getPosition());
   //   // }
-    
+
   //   // map.fitBounds(bounds);
-    
+
   // }
 }
